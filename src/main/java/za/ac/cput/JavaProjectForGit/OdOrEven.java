ecb84af40fc2b72151d45688651f6824cae87421
BroCodeSG <1stefangr@gmail.com>
2021-04-01 09:32:02 +0200

    Comitting

---
+++ src/main/java/za/ac/cput/JavaProjectForGit/OdOrEven.java
@@ -0,0 +1,20 @@
+package za.ac.cput.JavaProjectForGit;
+
+public class OdOrEven {
+
+    public String check(int val){
+ String ans="";
+      if(val%2==0){
+            ans="Even";
+
+      }
+      else {
+          ans="not even";
+
+      }
+
+        return ans;
+    }
+
+
+}
---
+++ src/test/java/za/ac/cput/JavaProjectForGit/OdOrEvenTest.java
@@ -0,0 +1,42 @@
+package za.ac.cput.JavaProjectForGit;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OdOrEvenTest {
+    @Test(timeout = 10000)
+
+    public void testAwnser(){
+        OdOrEven testItem = new OdOrEven();
+        String value=testItem.check(3);
+        assertEquals("not even",value);
+    }
+    @Ignore
+    @Test
+    public void checkExcistince(){
+        OdOrEven testItem = new OdOrEven();
+        String value=testItem.check(3);
+        assertNotNull(value);
+
+
+    }
+    @Test
+    public void minus(){
+        OdOrEven testItem = new OdOrEven();
+        String value=testItem.check(-3);
+        assertEquals("not even",value);
+        fail();
+
+
+    }
+    @Test
+    public void testIdentity(){
+        OdOrEven testItem = new OdOrEven();
+        String value=testItem.check(3);
+        assertSame("not even",value);
+    }
+
+
+}
