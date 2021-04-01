23080a3f7c8ff88e27378c1cd24d9d4fe0cb728b
BroCodeSG <1stefangr@gmail.com>
2021-04-01 12:35:32 +0200

    Comitting

--- src/main/java/za/ac/cput/JavaProjectForGit/OdOrEven.java
+++ src/main/java/za/ac/cput/JavaProjectForGit/OdOrEven.java
@@ -5,15 +5,15 @@ package za.ac.cput.JavaProjectForGit;
 public class OdOrEven {
 
     public String check(int val){
- String ans="";
-      if(val%2==0){
+        String ans="";
+        if(val%2==0){
             ans="Even";
 
-      }
-      else {
-          ans="not even";
+        }
+        else {
+            ans="not even";
 
-      }
+        }
 
         return ans;
     }
