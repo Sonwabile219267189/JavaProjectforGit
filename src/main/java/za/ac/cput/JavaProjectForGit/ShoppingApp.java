c115b67213c78eddd884f05851f9fb65666214e7
2172105135 <81383559+2172105135@users.noreply.github.com>
2021-04-01 17:58:17 +0200

    Create class and method to demonstrate two objects.

--- src/main/java/za/ac/cput/JavaProjectForGit/ShoppingApp.java
+++ src/main/java/za/ac/cput/JavaProjectForGit/ShoppingApp.java
@@ -7,4 +7,23 @@ package za.ac.cput.JavaProjectForGit;
  */
 
 public class ShoppingApp {
+
+
+    /*
+     * The class and methods below demonstrate two objects equality and identity.
+     */
+public static class Product{
+    public String itemName;
+    private double price;
+
+    public Product (String barCode, double price){
+        this.itemName = itemName;
+        this.price = price;
+
+    }
+    public boolean equals(Product p){
+        return (p.itemName == itemName) && (p.price == price);
+    }
+
+}
 }
