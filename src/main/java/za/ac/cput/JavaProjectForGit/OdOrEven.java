51595d588c68a7a337548fcccb02c9081c97d90a
Zuko <217299911@mycput.ac.za>
2021-04-01 22:49:46 +0200

    217299911 - Added a compare method

--- src/main/java/za/ac/cput/JavaProjectForGit/OdOrEven.java
+++ src/main/java/za/ac/cput/JavaProjectForGit/OdOrEven.java
@@ -2,6 +2,8 @@ package za.ac.cput.JavaProjectForGit;
 //Autohor Sn219104891
 //Funtions determine if int is odd or even
 
+import java.util.Scanner;
+
 public class OdOrEven {
 
     public String check(int val){
@@ -18,5 +20,23 @@ public class OdOrEven {
         return ans;
     }
 
+    //Author 217299911
+    //Function determines if number entered is lower or higher than set number
+
+    public String compare(int num){
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Please enter a number:");
+        num = reader.nextInt();
+
+        if(num>=10){
+           return "Number higher than 10";
+        }
+
+        else{
+            return "Number lower than 10";
+        }
+
+
+    }
 
 }
