c594d6789872bf82778c2e3c0f73b919b6b43a74
2172105135 <81383559+2172105135@users.noreply.github.com>
2021-04-01 18:13:50 +0200

    Initialized objects

--- src/main/java/za/ac/cput/JavaProjectForGit/ShoppingApp.java
+++ src/main/java/za/ac/cput/JavaProjectForGit/ShoppingApp.java
@@ -7,23 +7,36 @@ package za.ac.cput.JavaProjectForGit;
  */
 
 public class ShoppingApp {
+    public static void main(String[] args) {
 
+        Product p1 = new Product("Hand sanitizer",70.00);
+        Product p2 = new Product ("Mask",25.00);
 
-    /*
-     * The class and methods below demonstrate two objects equality and identity.
-     */
-public static class Product{
-    public String itemName;
-    private double price;
+        System.out.println(p1 == p2);//equality
+        System.out.println(p1.equals(p2)); // identity
 
-    public Product (String barCode, double price){
-        this.itemName = itemName;
-        this.price = price;
 
     }
-    public boolean equals(Product p){
-        return (p.itemName == itemName) && (p.price == price);
+
+        /*
+         * The class and methods below demonstrate two objects equality and identity.
+         */
+
+        public static class Product {
+            public String itemName;
+            private double price;
+
+            public Product(String itemName, double price) {
+                this.itemName = itemName;
+                this.price = price;
+
+            }
+
+            public boolean equals(Product p) {
+                return (p.itemName == itemName) && (p.price == price);
+            }
+
+        }
     }
 
-}
-}
+
